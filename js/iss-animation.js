a99bf9132bd141739e3738f9b824580c36d644c4
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-11-13 22:02:12 +0000

    Add transitions

--- js/iss-animation.js
+++ js/iss-animation.js
@@ -1,27 +1,29 @@
 const html = document.documentElement;
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext("2d");
+const issCont = document.querySelector('canvas');
 const numOfFrames = 150;
 
 function getCurrentFrame (index) {
     return `./media/iss-anim-sequence/${index.toString().padStart(4, '0')}.png`;
 }
 
-function preloadImages  () {
+function preloadImages () {
     for (let i=1; i<numOfFrames; i++) {
         const img = new Image();
         img.src = getCurrentFrame(i);
     }
 }
 
-canvas.width=1920;
-canvas.height=1080;
 
 const img = new Image();
 img.src = getCurrentFrame(1);
+canvas.width=1920;
+canvas.height=1080;
 
 img.onload = function() {
     context.drawImage(img, 0, 0);
+    issCont.classList.add('fade-in');
 }
 
 
@@ -32,15 +34,24 @@ function updateImage (frame) {
 }
 
 
-window.addEventListener('scroll', () => {
-    const animContainer = document.querySelector('.animation-cont');
+window.addEventListener('scroll', () => {  
     const scrollTop = html.scrollTop;
     const maxScrollTop = html.scrollHeight - window.innerHeight;
     const scrollFraction = scrollTop / maxScrollTop;
-    const frameIndex = Math.min( numOfFrames - 1,
-        Math.ceil(scrollFraction * numOfFrames));
-
-    requestAnimationFrame(() => updateImage(frameIndex + 1))
-})
+    const frameIndex = Math.min(
+      numOfFrames - 1,
+      Math.ceil(scrollFraction * numOfFrames)
+    );
+    
+    requestAnimationFrame(() => updateImage(frameIndex + 1));
+  });
 
 preloadImages();
+
+
+// Page animation
+const h1 = document.querySelector('h1');
+
+document.addEventListener('DOMContentLoaded', () => {
+    h1.classList.add('fade-up');
+})
